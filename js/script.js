836442700af28983183e1f85b5e4da9e95a7055b
Cтас <90937465+sfirsovdev@users.noreply.github.com>
2022-04-07 04:37:03 +0300

    Моб.адаптация
    Закончил блок аккордион

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.slider').slick({
         arrows: true,
         dots: true,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
     }
     );  
 });
@@ -11,7 +11,7 @@ $(document).ready(function(){
     $('.slider2').slick({
         arrows: true,
         dots:false,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         vertical: true,
         slidesToShow: 3,
         asNavFor: ".sliderbig",
@@ -24,7 +24,7 @@ $(document).ready(function(){
     $('.sliderbig').slick({
         arrows: false,
         dots: false,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         slidesToShow: 1,
         fade: true,
         asNavFor:".slider2"
